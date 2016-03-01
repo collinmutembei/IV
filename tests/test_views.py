5277cce82b5eb0df8e67f1665a1e2d75a1cd6789
Collin Mutembei <mutembei.collin@gmail.com>
2016-03-01 22:09:55 +0300

    [chore] update tests
    - change templates to only load app, and
      require user to login if need be
    - fix tests for views to raise coverage
    - include documentation for tests to fix
      complains on quantifiedcode
    - use a bigger favicon

--- tests/test_views.py
+++ tests/test_views.py
@@ -2,11 +2,15 @@ from django.test import TestCase
 
 
 class PheditViewTestCase(TestCase):
-    def test_landing_page_view(self):
-        resp = self.client.get('/')
+    """define tests for views"""
+
+    def test_dashboard_page_view(self):
+        """test behaviour on accessing app dashboard"""
+        resp = self.client.get('/web/')
         self.assertEqual(resp.status_code, 200)
         self.assertContains(resp, 'PHEDIT')
 
-    def test_dashboard_view_when_not_authenticated(self):
-        resp = self.client.get('/dashboard/')
+    def test_landing_page_view(self):
+        """test accessing the landing page"""
+        resp = self.client.get('/')
         self.assertEqual(resp.status_code, 302)
