3b73891ccaafabeb60c7b9597bb222a4c43b1c00
Collin Mutembei <mutembei.collin@gmail.com>
2016-03-21 23:54:15 +0300

    [feture] integrate with auth0
    - rely on auth0 for easy user signup as
      well as levearging social auth

--- app/templates/static/scripts/app.js
+++ app/templates/static/scripts/app.js
@@ -1,10 +1,21 @@
-angular.module('pheditApp', ['ngRoute']);
+angular.module('pheditApp', ['ngRoute', 'auth0', 'angular-storage', 'angular-jwt']);
+
+angular.module('pheditApp').config(function (authProvider) {
+  authProvider.init({
+    domain: 'solublecode.auth0.com',
+    clientID: '0RobeRw5JMnTMxtxzLAm3RxGhLQbyzN4'
+  });
+})
+.run(function(auth) {
+  // This hooks all auth events to check everything as soon as the app starts
+  auth.hookEvents();
+});
 
 angular.module('pheditApp').config(function($routeProvider){
     $routeProvider
         .when('/', {
             templateUrl: 'static/pages/landing.html'
-        }).when('/app/', {
+        }).when('/app', {
             templateUrl: 'static/pages/dashboard.html'
         });
 });
--- app/templates/static/scripts/controllers/controller.js
+++ app/templates/static/scripts/controllers/controller.js
@@ -1,1 +1,21 @@
-angular.module('pheditApp').controller('mainCtrl');
+angular.module('pheditApp').controller('mainCtrl', ['$scope', '$http', 'auth', 'store', '$location',
+function ($scope, $http, auth, store, $location) {
+
+  $scope.login = function () {
+    auth.signin({}, function (profile, token) {
+      // Success callback
+      store.set('profile', profile);
+      store.set('token', token);
+      $location.path('/app');
+    }, function () {
+      // Error callback
+    });
+  }
+
+  $scope.logout = function() {
+    auth.signout();
+    store.remove('profile');
+    store.remove('token');
+  }
+
+ }]);
