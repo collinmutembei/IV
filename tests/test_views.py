c3287f3ab8c8326b6e8b828b95a8c0c144620bfc
Collin Mutembei <mutembei.collin@gmail.com>
2016-03-22 00:09:21 +0300

    [fix] fix failing build
    - update tests to reflect changes in views

--- tests/test_views.py
+++ tests/test_views.py
@@ -6,11 +6,11 @@ class PheditViewTestCase(TestCase):
 
     def test_dashboard_page_view(self):
         """test behaviour on accessing app dashboard"""
-        resp = self.client.get('/web/')
+        resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
         self.assertContains(resp, 'PHEDIT')
 
     def test_landing_page_view(self):
         """test accessing the landing page"""
-        resp = self.client.get('/')
-        self.assertEqual(resp.status_code, 302)
+        resp = self.client.get('/xyzabcd')
+        self.assertEqual(resp.status_code, 404)
